693b6eed18024f5c87539956a110080a415a26cb
yeeim <yerimyoon1024@gmail.com>
2026-07-20 11:17:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.markdown("# 앱UI만들기")
 name = st.text_input("이름", placeholder="example_user")
-grade = st.radio_input("학년", [1,2,3], horizontal=True)
+grade = st.radio("학년", [1,2,3], horizontal=True)
 clas = st.number_input("반")
 level =st.selct_slider("난이도, [쉬움, 보통, 어려움]")
 score = st.select_slider("점수", [0, 50, 100])
