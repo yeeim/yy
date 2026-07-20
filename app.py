e2313f3e8fa854851af8c8acf97cb9da2226f071
yeeim <yerimyoon1024@gmail.com>
2026-07-20 11:40:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-st.markdown("# 앱UI만들기")
-name = st.text_input("이름", placeholder="example_user")
-grade = st.radio("학년", [1,2,3], horizontal=True)
-clas = st.number_input("반")
-level =st.selct_slider("난이도, [쉬움, 보통, 어려움]")
-score = st.select_slider("점수", [0, 50, 100])
-소감 =st.text_area("소감")
+with st.sidebar:
+    st.header("프로필")
+    user_name = st.text-input("닉네임")
+   weater = st. selectbox("오늘 날씨", ["맑음", "흐림", '비/눈", "메우 추움"])
+    st.markdoowm("---")
+    st.info(f"반가워요, {user_name}님! 오늘날씨는 '{weater}'dlspdy.")
+
+                                    st.title("AI코디메이커")
+                                    st.write("사이드바에서 날씨를 먼저선택하고 코디를 시작하세요!")   
