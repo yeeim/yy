aa58db48160854122198a11031dc9c213385b481
yeeim <yerimyoon1024@gmail.com>
2026-07-20 14:24:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.header("디테일 추가")
 tab1, tab2=st.tabs(["신발", "엑세서리"])
 with tab1: 
      st.write=("오늘의 발걸음을 책임질 신발")
-     shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두, "슬리퍼"],key="shoes")
+     shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두", "슬리퍼"],key="shoes")
      with st.expander("신발 선택 팁 보기"):
           st.info("너무 튀는 신발은 지양하도록해요!")
 with tab2:
