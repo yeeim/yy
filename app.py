573b0e62fc0de9ad679fc9df3a55a5d9e1d80ba8
yeeim <yerimyoon1024@gmail.com>
2026-07-20 13:17:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 with st.sidebar:
     st.header("프로필")
     user_name = st.text-input("닉네임")
-   weater = st. selectbox("오늘 날씨", ["맑음", "흐림", '비/눈", "메우 추움"])
+   weater = st.selectbox("오늘 날씨", ["맑음", "흐림", '비/눈", "메우 추움"])
     st.markdoowm("---")
     st.info(f"반가워요, {user_name}님! 오늘날씨는 '{weater}'dlspdy.")
 
