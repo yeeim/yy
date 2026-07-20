7c1ceb33b3258e6a93add9b9cbfd1fea2889d048
yeeim <yerimyoon1024@gmail.com>
2026-07-20 14:22:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def reset_all():
 with st.sidebar:
     st.header("프로필")
     user_name = st.text-input("닉네임", key="user_name")
-   weather = st.selectbox("오늘 날씨", ["맑음", "흐림", '비/눈", "매우 추움"], key=weather)
+   weather = st.selectbox("오늘 날씨", ["맑음", "흐림", '비/눈", "매우 추움"], key="weather")
     st.markdoowm("---")
     st.info(f"반가워요, {user_name}님! 오늘날씨는 '{weather}'이네요.")
 
@@ -24,37 +24,38 @@ with st.sidebar:
     coll, col2 = st.columns
 with col1:
         st.subheader("상의")
-        top_type = st.radio("종류"< ["후두티", "셔츠", "맨투맨", "반팔 티셔츠"], key=top_type)
-        top_color = st.select_slider('색상 톤", option=["밝음", "무난함", "어두움"], key=top_color)
+        top_type = st.radio("종류"< ["후두티", "셔츠", "맨투맨", "반팔 티셔츠"], key="top_type")
+        top_color = st.select_slider('색상 톤", option=["밝음", "무난함", "어두움"], key="top_color")
+                                     
 with col2: 
        st.subheader("하의")
-       bottom_type st.radio("종류", ["청바지", "슬랙스","트레이닝 팬츠","반바지"], key=bottom_type)
-       bottom_color=st.select_slider("핏(fit)", options=["슬림", "레귤러", "오버핏"], key=bottom_color)
+       bottom_type st.radio("종류", ["청바지", "슬랙스","트레이닝 팬츠","반바지"], key="bottom_type")
+       bottom_color=st.select_slider("핏(fit)", options=["슬림", "레귤러", "오버핏"], key="bottom_color")
 
 st.header('디테일 추가")
 tab1, tab2=st.tabs(["신발", "엑세서리"])
 with tab1: 
-st.write=("오늘의 발걸음을 책임질 신발")
-shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두, "슬리퍼"],key=shoes)
-with st.expander("신발 선택 팁 보기"):
-st.info("너무 튀는 신발은 지양하도록해요!")
+     st.write=("오늘의 발걸음을 책임질 신발")
+     shoes = st.selectbox("신발 선택", ["스니커즈", "운동화", "구두, "슬리퍼"],key="shoes")
+     with st.expander("신발 선택 팁 보기"):
+          st.info("너무 튀는 신발은 지양하도록해요!")
 with tab2:
-st.write("포인트 아이템:")
-acc+ st.multiselect("액세사리 스티일링 팁 보기"):
-st.warning("너무 많은 액세서리는 투머치가 될 수 있어요")
+     st.write("포인트 아이템:")
+     acc= st.multiselect("액세사리 스티일링 팁 보기"):
+     st.warning("너무 많은 액세서리는 투머치가 될 수 있어요")
 st.markdown("---")
 if st.button("코디 완성하기"):
- with st.container(border=Ture):
- st.sunheader(f"{user_name}님의 오늘의 축복")
- st.write(f"오늘 같은**{weatehr}**날씨에는 이렇게 입어보세요!")
- st.markdown(f"""
- * **상의:**{top_color} {top_type}
- * **하의:**{bottom-color} {bottom_type}
- * **매칭:**{shoes}와 {', '.join(acc) if acc else'악세사리 없이 깔끔하게!'}
- """)
- st.success("오늘의 스타일링이 완성되었습니다! 자신 있게외출하세요!")
-wirh st.expander('코디 연출 팁 영상 보기"):
-     st.video("https://www.youtube.com/watch?v-1kmz8yt1y1k")
-     st.write("전문가가 제안하는 코디 연출법을 참고해 보세요")
+     with st.container(border=Ture):
+          st.sunheader(f"{user_name}님의 오늘의 축복")
+          st.write(f"오늘 같은**{weatehr}**날씨에는 이렇게 입어보세요!")
+          st.markdown(f"""
+          * **상의:**{top_color} {top_type}
+          * **하의:**{bottom-color} {bottom_type}
+          * **매칭:**{shoes}와 {', '.join(acc) if acc else'악세사리 없이 깔끔하게!'}
+          """)
+          st.success("오늘의 스타일링이 완성되었습니다! 자신 있게외출하세요!")
+          with st.expander('코디 연출 팁 영상 보기"):
+               st.video("https://www.youtube.com/watch?v-1kmz8yt1y1k")
+               st.write("전문가가 제안하는 코디 연출법을 참고해 보세요")
 
-     st.button("전체 초기화", on_click=reset=all)
+st.button("전체 초기화", on_click=reset=all)
