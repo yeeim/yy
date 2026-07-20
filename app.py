017644f394cf166a9932b6b391b0016be8e51dbe
yeeim <yerimyoon1024@gmail.com>
2026-07-20 14:07:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 def reset_all():
      st.session_state.user_name = ""
      st.session_state.weather = "맑음"
-     st.session_state.top_type = : "후두티"
+     st.session_state.top_type = "후두티"
      st.session_state.top_color = "밝음"
      st.session_state.bottom_type = "청바지"
      st.session_state.bottom_color = "슬림"
