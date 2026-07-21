d91c6b9ffacbc4ca6fd1964502350ce9997e9d01
yeeim <yerimyoon1024@gmail.com>
2026-07-21 10:15:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+from openar import OpenAI
+ai_client = OpenAI(api_key=st.secrets["OPENAI-API-KEY"])
+
 if 'todo_list' not in st.session_state:
     st.session_state.todo_list = []
 if 'user_motto' not in st.session_state:
@@ -74,6 +77,16 @@ def page_report():
             st.session_state.todo_list = []
             st.rerun()
 
+def page_ai_coach():
+    st.header("AI코치와 대화하기")
+    prompt=st.text_input("질문을 입력하세요")
+    if st.button("보내기"):
+        reponse=ar_client.respones.create(
+            model=gpt-5.4-mini,
+            input=prompt
+        )
+        st.write(response.out_text)
+        
 pg = st.navigation([
     st.Page(page_motto, title="오늘의 다짐", icon="📣"),
     st.Page(page_todo, title="오늘의 할 일", icon="✅"),
