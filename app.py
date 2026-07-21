a1b9abc389a1070014033967dc81d6d1ce5d78de
yeeim <yerimyoon1024@gmail.com>
2026-07-21 10:15:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from openai import OpenAI
-ai_client = OpenAI(api_key=st.secrets["OPENAI-API-KEY"])
+ai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if 'todo_list' not in st.session_state:
     st.session_state.todo_list = []
